0135f056b461f77c98b6950bfe5f501328ef906f
JaySeteni <jay.seteni@gmail.com>
2024-06-28 16:52:01 +0200

    back button function

--- src/app/comps/top-nav/top-nav.component.ts
+++ src/app/comps/top-nav/top-nav.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import  { Location } from '@angular/common'
 
 @Component({
   selector: 'app-top-nav',
   templateUrl: './top-nav.component.html',
   styleUrl: './top-nav.component.css'
 })
-export class TopNavComponent {
+export class TopNavComponent implements OnInit {
 
+  constructor(private location : Location ){}
+
+    ngOnInit(): void {
+    }
+  
+
+  goBack(){
+    this.location.back()
+  }
 }
