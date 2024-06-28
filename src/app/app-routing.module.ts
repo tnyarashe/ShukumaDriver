a23cf775a7cb02db6306172529a75090326c760d
JaySeteni <jay.seteni@gmail.com>
2024-06-28 14:08:38 +0200

    pushing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopNavComponent } from './comps/top-nav/top-nav.component';
+import { BottomNavComponent } from './comps/bottom-nav/bottom-nav.component';
+import { HomeComponent } from './comps/home/home.component';
+import { RegisterComponent } from './comps/register/register.component';
+import { LoginComponent } from './comps/login/login.component';
+import { OrdersComponent } from './comps/orders/orders.component';
+import { ViewordersComponent } from './comps/vieworders/vieworders.component';
+import { SettingsComponent } from './comps/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'topnav', component: TopNavComponent},
+  {path: 'bottomnav', component: BottomNavComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'orders', component: OrdersComponent},
+  {path: 'order', component: ViewordersComponent},
+  {path: 'settings', component: SettingsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
