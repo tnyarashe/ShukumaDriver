707155b1e414bd63bf24435abb90a2e6a4f73c92
DiniJNS <jonas.dinilethu@gmail.com>
2024-07-01 12:43:50 +0200

    finished chat

--- src/app/comps/chat/chat.component.ts
+++ src/app/comps/chat/chat.component.ts
@@ -1,31 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+// driver-chat.component.ts
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
-interface ChatMessage {
+interface Message {
+  sender: 'driver' | 'client';
   text: string;
-  sentByDriver: boolean;
+  avatar: string;
 }
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrls: ['./chat.component.css']
 })
-export class DriverChatComponent implements OnInit {
+export class DriverChatComponent {
+  @ViewChild('conversation', { static: false }) conversation: ElementRef | undefined;
 
-  messages: string[] = []; // Array to store chat messages as strings
-  newMessage: string = ''; // Input field for new messages
+  messages: Message[] = [
+    { sender: 'driver', text: 'Hi there, I\'m on my way and should be with you in about 5 minutes.', avatar: 'https://randomuser.me/api/portraits/men/12.jpg' },
+    { sender: 'client', text: 'Sounds good, I\'ll be waiting outside.', avatar: 'https://randomuser.me/api/portraits/women/12.jpg' },
+    { sender: 'driver', text: 'Great, I\'m pulling up now. I\'m in the black sedan.', avatar: 'https://randomuser.me/api/portraits/men/12.jpg' },
+    { sender: 'client', text: 'Okay, I\'m heading out. Be there in a sec.', avatar: 'https://randomuser.me/api/portraits/women/12.jpg' }
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
-    // Call service to get initial messages (if applicable)
-  }
+  newMessage = '';
 
   sendMessage() {
     if (this.newMessage.trim() !== '') {
-      // Send message using chat service
-      this.messages.push(this.newMessage + ' (driver)'); // Add sender indication
+      this.messages.push({
+        sender: 'client',
+        text: this.newMessage,
+        avatar: 'https://randomuser.me/api/portraits/women/12.jpg'
+      });
       this.newMessage = '';
+      // this.scrollToBottom();
     }
   }
-  }
+
+  // scrollToBottom() {
+  //   setTimeout(() => {
+  //     this.conversation.nativeElement.scrollTop = this.conversation.nativeElement.scrollHeight;
+  //   }, 100);
+  // }
+}
